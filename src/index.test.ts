a2a3a0d6daf7c78e36aa4cbcb8a05da86610406a
Masaki Hara <ackie.h.gmai@gmail.com>
2023-01-07 17:03:29 +0900

    Add ordinary template tag

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,17 @@
-import { expect, test } from "@jest/globals";
+import { describe, expect, it, test } from "@jest/globals";
+import { template } from "./index.js";
 
-test("foo", () => {
-  expect(1 + 1).toBe(2);
+describe("template", () => {
+  it("interpolates a string", () => {
+    expect(template`abc${"def"}ghi`).toBe("abcdefghi");
+  });
+  it("interpolates a number", () => {
+    expect(template`abc${123}ghi`).toBe("abc123ghi");
+  });
+  it("does not interpolate a symbol", () => {
+    expect(() => template`abc${Symbol.iterator}ghi`).toThrow(TypeError);
+  });
+  it("interprets escape", () => {
+    expect(template`a\nb`).toBe("a\nb");
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,48 @@
-export function dedent() {}
+/*
+export type TemplateTag<S extends any[], T> =
+  (template: TemplateStringsArray, ...substitutions: S) => T;
+
+export function dedent(
+  template: TemplateStringsArray,
+  ...substitutions: unknown[]
+): string;
+export function dedent<S extends any[], T>(
+  innerTag: TemplateTag<S, T>
+): TemplateTag<S, T>;
+export function dedent(
+  template1: any,
+  ...substitutions: any
+): any {
+  if (typeof template1 === "function") {
+    return dedentInner(template1);
+  } else {
+    return dedentInner(template)(template1, ...substitutions);
+  }
+}
+
+function dedentInner<S extends any[], T>(
+  innerTag: TemplateTag<S, T>
+): TemplateTag<S, T> {
+}
+*/
+
+export function template(
+  template: TemplateStringsArray,
+  ...substitutions: unknown[]
+): string {
+  let result = "";
+  for (let nextIndex = 0; nextIndex < template.length; nextIndex++) {
+    result += template[nextIndex];
+    if (nextIndex < substitutions.length) {
+      result += toString(substitutions[nextIndex]);
+    }
+  }
+  return result;
+}
+
+function toString(obj: unknown): string {
+  if (typeof obj === "symbol") {
+    throw new TypeError("Cannot convert a Symbol value to a string");
+  }
+  return String(obj);
+}
