820912e093784c72ceda2df28b00d4f407a93803
Masaki Hara <ackie.h.gmai@gmail.com>
2023-01-14 12:41:46 +0900

    Split dedentRaw.ts

---
+++ src/dedentRaw.test.ts
@@ -0,0 +1,74 @@
+import { describe, expect, it } from "@jest/globals";
+import { dedentRaw } from "./dedentRaw.js";
+
+describe("dedentRaw", () => {
+  it("removes initial spaces", () => {
+    expect(dedentRaw(["    foo bar "])).toEqual(["foo bar "]);
+  });
+  it("removes initial tabs", () => {
+    expect(dedentRaw(["\tfoo\tbar\t"])).toEqual(["foo\tbar\t"]);
+  });
+  it("removes the first line if it is empty", () => {
+    expect(dedentRaw(["\nfoo bar "])).toEqual(["foo bar "]);
+  });
+  it("removes the first line if it only contains spaces", () => {
+    expect(dedentRaw(["    \nfoo bar "])).toEqual(["foo bar "]);
+  });
+  it("removes the first line if it only contains tabs", () => {
+    expect(dedentRaw(["\t\nfoo bar "])).toEqual(["foo bar "]);
+  });
+  it("keeps the last newlines", () => {
+    expect(dedentRaw(["x\n\n\n"])).toEqual(["x\n\n\n"]);
+  });
+  it("keeps spaces after substitution", () => {
+    expect(dedentRaw(["  foo", " bar"])).toEqual(["foo", " bar"]);
+  });
+  it("keeps spaces after bar", () => {
+    expect(dedentRaw(["  foo", " bar"])).toEqual(["foo", " bar"]);
+  });
+  it("dedents the second line (without substitution)", () => {
+    expect(dedentRaw(["foo\n  bar "])).toEqual(["foo\nbar "]);
+  });
+  it("dedents the second line (with substitution)", () => {
+    expect(dedentRaw(["foo", "foo\n  bar "])).toEqual(["foo", "foo\nbar "]);
+  });
+  it("dedents the second line, tab case (without substitution)", () => {
+    expect(dedentRaw(["foo\n\t\tbar\t"])).toEqual(["foo\nbar\t"]);
+  });
+  it("dedents the second line, tab case (with substitution)", () => {
+    expect(dedentRaw(["foo", "foo\n\t\tbar\t"])).toEqual(["foo", "foo\nbar\t"]);
+  });
+  it("dedents with variable indentation 1", () => {
+    expect(dedentRaw(["foo\nbar"])).toEqual(["foo\nbar"]);
+  });
+  it("dedents with variable indentation 2", () => {
+    expect(dedentRaw(["foo\n bar"])).toEqual(["foo\nbar"]);
+  });
+  it("dedents with variable indentation 3", () => {
+    expect(dedentRaw(["foo\n  bar"])).toEqual(["foo\nbar"]);
+  });
+  it("dedents with variable indentation 4", () => {
+    expect(dedentRaw(["foo\n   bar"])).toEqual(["foo\nbar"]);
+  });
+  it("dedents lines using max common indent 1", () => {
+    expect(dedentRaw(["foo\n  bar\n    baz"])).toEqual(["foo\nbar\n  baz"]);
+  });
+  it("dedents lines using max common indent 2", () => {
+    expect(dedentRaw(["foo\n    bar\n  baz"])).toEqual(["foo\n  bar\nbaz"]);
+  });
+  it("truncates short empty line in the middle", () => {
+    expect(dedentRaw(["foo\n \n  bar\n    baz"])).toEqual(["foo\n\nbar\n  baz"]);
+  });
+  it("truncates short empty line in the last", () => {
+    expect(dedentRaw(["foo\n  bar\n    baz\n "])).toEqual(["foo\nbar\n  baz\n"]);
+  });
+  it("regards substitution as a content", () => {
+    expect(dedentRaw(["foo\n ", "\n  bar\n    baz"])).toEqual(["foo\n", "\n bar\n   baz"]);
+  });
+  it("dedents long empty line", () => {
+    expect(dedentRaw(["foo\n   \n  bar\n    baz"])).toEqual(["foo\n \nbar\n  baz"]);
+  });
+  it("dedents the second line and later at Infinity if they are all empty", () => {
+    expect(dedentRaw(["x\n  \u2028\t\n   \u2029\t\t"])).toEqual(["x\n\u2028\n\u2029"]);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -49,37 +49,6 @@ function dedentTemplateImpl(
 }
 */
 
-export function dedentRaw(raw: readonly string[]): string[] {
-  // Dedent the second line and later
-  let level = Infinity;
-  for (let i = 0; i < raw.length; i++) {
-    const elem = raw[i]!;
-    const re = /[\n\u2028\u2029]([ \t]*)/g;
-    while (true) {
-      const m = re.exec(elem);
-      if (!m) {
-        break;
-      }
-      const endPos = m.index + m[0].length;
-      const len = m[1]!.length;
-      const hasContent = elem[endPos] != null ?
-        // There is a character in the line
-        !/[\n\u2028\u2029]/.test(elem[endPos]!) :
-        // There is a substitution
-        i + 1 < raw.length;
-      if (hasContent) {
-        level = Math.min(level, len);
-      }
-    }
-  }
-  const dedented = raw.map((elem) => elem.replace(/([\n\u2028\u2029])([ \t]*)/g, (text, nl: string, head: string) => nl + head.substring(level)));
-
-  // Dedent the first line and remove it if it is empty
-  // First line indentation is independent of the later lines.
-  dedented[0] = dedented[0]!.replace(/^[ \t]*[\n\u2028\u2029]?/, "");
-  return dedented;
-}
-
 export function template(
   template: TemplateStringsArray,
   ...substitutions: unknown[]
