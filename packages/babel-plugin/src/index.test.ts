46af9ec98487b993ff2276954605066b52fcaa38
Masaki Hara <ackie.h.gmai@gmail.com>
2023-02-12 17:57:03 +0900

    Rearrange tests

--- packages/babel-plugin/src/index.test.ts
+++ packages/babel-plugin/src/index.test.ts
@@ -15,227 +15,236 @@ describe("@qnighy/babel-plugin-dedent", () => {
     }
     return result.code ?? "";
   }
-  it("transforms dedent calls", async () => {
-    const input = dedent`
-      import { dedent } from "@qnighy/dedent";
-      const text = dedent\`
-        foo
+
+  describe("Basic behavior", () => {
+    it("transforms dedent calls", async () => {
+      const input = dedent`
+        import { dedent } from "@qnighy/dedent";
+        const text = dedent\`
+          foo
+          bar
+        \`;
+      `;
+      // TODO: remove imports
+      const output = dedent`
+        import { dedent } from "@qnighy/dedent";
+        const text = \`foo
         bar
-      \`;
-    `;
-    // TODO: remove imports
-    const output = dedent`
-      import { dedent } from "@qnighy/dedent";
-      const text = \`foo
-      bar
-      \`;`;
-    expect(await transform(input)).toBe(output);
-  });
-  it("transforms wrapping dedent calls", async () => {
-    const input = dedent`
-      import { dedent } from "@qnighy/dedent";
-      const text = dedent(foo)\`
-        foo
+        \`;`;
+      expect(await transform(input)).toBe(output);
+    });
+    it("transforms wrapping dedent calls", async () => {
+      const input = dedent`
+        import { dedent } from "@qnighy/dedent";
+        const text = dedent(foo)\`
+          foo
+          bar
+        \`;
+      `;
+      // TODO: remove imports
+      const output = dedent`
+        import { dedent } from "@qnighy/dedent";
+        const text = foo\`foo
         bar
-      \`;
-    `;
-    // TODO: remove imports
-    const output = dedent`
-      import { dedent } from "@qnighy/dedent";
-      const text = foo\`foo
-      bar
-      \`;`;
-    expect(await transform(input)).toBe(output);
+        \`;`;
+      expect(await transform(input)).toBe(output);
+    });
   });
 
-  it("ignores other template literals", async () => {
-    const input = dedent`
-      \`
-        foo
+  describe("dedent detection", () => {
+    it("ignores other template literals", async () => {
+      const input = dedent`
+        \`
+          foo
+          bar
+        \`;
+        foo\`
+          foo
+          bar
+        \`;
+        foo.bar\`
+          foo
+          bar
+        \`;
+        foo(bar)\`
+          foo
+          bar
+        \`;
+        foo()\`
+          foo
+          bar
+        \`;
+      `;
+      const output = dedent`
+        \`
+          foo
+          bar
+        \`;
+        foo\`
+          foo
+          bar
+        \`;
+        foo.bar\`
+          foo
+          bar
+        \`;
+        foo(bar)\`
+          foo
+          bar
+        \`;
+        foo()\`
+          foo
+          bar
+        \`;`;
+      expect(await transform(input)).toBe(output);
+    });
+
+    it("detects renamed imports", async () => {
+      const input = dedent`
+        import { dedent as m } from "@qnighy/dedent";
+        const text = m\`
+          foo
+          bar
+        \`;
+      `;
+      const output = dedent`
+        import { dedent as m } from "@qnighy/dedent";
+        const text = \`foo
         bar
-      \`;
-      foo\`
-        foo
+        \`;`;
+      expect(await transform(input)).toBe(output);
+    });
+
+    it("detects string imports", async () => {
+      const input = dedent`
+        import { "dedent" as dedent } from "@qnighy/dedent";
+        const text = dedent\`
+          foo
+          bar
+        \`;
+      `;
+      const output = dedent`
+        import { "dedent" as dedent } from "@qnighy/dedent";
+        const text = \`foo
         bar
-      \`;
-      foo.bar\`
-        foo
-        bar
-      \`;
-      foo(bar)\`
-        foo
-        bar
-      \`;
-      foo()\`
-        foo
-        bar
-      \`;
-    `;
-    const output = dedent`
-      \`
-        foo
-        bar
-      \`;
-      foo\`
-        foo
-        bar
-      \`;
-      foo.bar\`
-        foo
-        bar
-      \`;
-      foo(bar)\`
-        foo
-        bar
-      \`;
-      foo()\`
-        foo
-        bar
-      \`;`;
-    expect(await transform(input)).toBe(output);
+        \`;`;
+      expect(await transform(input)).toBe(output);
+    });
+
+    it("ignores global dedent", async () => {
+      const input = dedent`
+        const text = dedent\`
+          foo
+          bar
+        \`;
+      `;
+      const output = dedent`
+        const text = dedent\`
+          foo
+          bar
+        \`;`;
+      expect(await transform(input)).toBe(output);
+    });
+
+    it("ignores other imports", async () => {
+      const input = dedent`
+        import { dedentRaw as dedent } from "@qnighy/dedent";
+        const text = dedent\`
+          foo
+          bar
+        \`;
+      `;
+      const output = dedent`
+        import { dedentRaw as dedent } from "@qnighy/dedent";
+        const text = dedent\`
+          foo
+          bar
+        \`;`;
+      expect(await transform(input)).toBe(output);
+    });
   });
 
-  it("detects renamed imports", async () => {
-    const input = dedent`
-      import { dedent as m } from "@qnighy/dedent";
-      const text = m\`
-        foo
-        bar
-      \`;
-    `;
-    const output = dedent`
-      import { dedent as m } from "@qnighy/dedent";
-      const text = \`foo
-      bar
-      \`;`;
-    expect(await transform(input)).toBe(output);
-  });
-
-  it("detects string imports", async () => {
-    const input = dedent`
-      import { "dedent" as dedent } from "@qnighy/dedent";
-      const text = dedent\`
-        foo
-        bar
-      \`;
-    `;
-    const output = dedent`
-      import { "dedent" as dedent } from "@qnighy/dedent";
-      const text = \`foo
-      bar
-      \`;`;
-    expect(await transform(input)).toBe(output);
-  });
-
-  it("ignores global dedent", async () => {
-    const input = dedent`
-      const text = dedent\`
-        foo
-        bar
-      \`;
-    `;
-    const output = dedent`
-      const text = dedent\`
-        foo
-        bar
-      \`;`;
-    expect(await transform(input)).toBe(output);
-  });
-
-  it("ignores other imports", async () => {
-    const input = dedent`
-      import { dedentRaw as dedent } from "@qnighy/dedent";
-      const text = dedent\`
-        foo
-        bar
-      \`;
-    `;
-    const output = dedent`
-      import { dedentRaw as dedent } from "@qnighy/dedent";
-      const text = dedent\`
-        foo
-        bar
-      \`;`;
-    expect(await transform(input)).toBe(output);
-  });
-
-  it("throws an error if there is an invalid escape in the direct form", async () => {
-    expect.assertions(1);
-    const input = dedent`
-      import { dedent } from "@qnighy/dedent";
-      const text = dedent\`
-        foo
+  describe("escape handling", () => {
+    it("throws an error if there is an invalid escape in the direct form", async () => {
+      expect.assertions(1);
+      const input = dedent`
+        import { dedent } from "@qnighy/dedent";
+        const text = dedent\`
+          foo
+          bar\\9
+        \`;
+      `;
+      await expect(transform(input)).rejects.toEqual(expect.objectContaining({
+        message: expect.stringMatching(/\\8 and \\9 are not allowed in template strings./),
+      }));
+    });
+    it("transforms the code successfully even if there is an invalid escape in the wrapper form", async () => {
+      const input = dedent`
+        import { dedent } from "@qnighy/dedent";
+        const text = dedent(foo)\`
+          foo
+          bar\\9
+        \`;
+      `;
+      const output = dedent`
+        import { dedent } from "@qnighy/dedent";
+        const text = foo\`foo
         bar\\9
-      \`;
-    `;
-    await expect(transform(input)).rejects.toEqual(expect.objectContaining({
-      message: expect.stringMatching(/\\8 and \\9 are not allowed in template strings./),
-    }));
-  });
-  it("transforms the code successfully even if there is an invalid escape in the wrapper form", async () => {
-    const input = dedent`
-      import { dedent } from "@qnighy/dedent";
-      const text = dedent(foo)\`
-        foo
-        bar\\9
-      \`;
-    `;
-    const output = dedent`
-      import { dedent } from "@qnighy/dedent";
-      const text = foo\`foo
-      bar\\9
-      \`;`;
-    expect(await transform(input)).toBe(output);
+        \`;`;
+      expect(await transform(input)).toBe(output);
+    });
   });
 
-  it("ignores wrapping dedent calls with too few arguments", async () => {
-    const input = dedent`
-      import { dedent } from "@qnighy/dedent";
-      const text = dedent()\`
-        foo
-        bar
-      \`;
-    `;
-    const output = dedent`
-      import { dedent } from "@qnighy/dedent";
-      const text = dedent()\`
-        foo
-        bar
-      \`;`;
-    expect(await transform(input)).toBe(output);
-  });
+  describe("parsing wrapped expressions", () => {
+    it("ignores wrapping dedent calls with too few arguments", async () => {
+      const input = dedent`
+        import { dedent } from "@qnighy/dedent";
+        const text = dedent()\`
+          foo
+          bar
+        \`;
+      `;
+      const output = dedent`
+        import { dedent } from "@qnighy/dedent";
+        const text = dedent()\`
+          foo
+          bar
+        \`;`;
+      expect(await transform(input)).toBe(output);
+    });
 
-  it("ignores wrapping dedent calls with too many arguments", async () => {
-    const input = dedent`
-      import { dedent } from "@qnighy/dedent";
-      const text = dedent(foo, bar)\`
-        foo
-        bar
-      \`;
-    `;
-    const output = dedent`
-      import { dedent } from "@qnighy/dedent";
-      const text = dedent(foo, bar)\`
-        foo
-        bar
-      \`;`;
-    expect(await transform(input)).toBe(output);
-  });
+    it("ignores wrapping dedent calls with too many arguments", async () => {
+      const input = dedent`
+        import { dedent } from "@qnighy/dedent";
+        const text = dedent(foo, bar)\`
+          foo
+          bar
+        \`;
+      `;
+      const output = dedent`
+        import { dedent } from "@qnighy/dedent";
+        const text = dedent(foo, bar)\`
+          foo
+          bar
+        \`;`;
+      expect(await transform(input)).toBe(output);
+    });
 
-  it("ignores wrapping dedent calls with spreads", async () => {
-    const input = dedent`
-      import { dedent } from "@qnighy/dedent";
-      const text = dedent(...foo)\`
-        foo
-        bar
-      \`;
-    `;
-    const output = dedent`
-      import { dedent } from "@qnighy/dedent";
-      const text = dedent(...foo)\`
-        foo
-        bar
-      \`;`;
-    expect(await transform(input)).toBe(output);
+    it("ignores wrapping dedent calls with spreads", async () => {
+      const input = dedent`
+        import { dedent } from "@qnighy/dedent";
+        const text = dedent(...foo)\`
+          foo
+          bar
+        \`;
+      `;
+      const output = dedent`
+        import { dedent } from "@qnighy/dedent";
+        const text = dedent(...foo)\`
+          foo
+          bar
+        \`;`;
+      expect(await transform(input)).toBe(output);
+    });
   });
 });
