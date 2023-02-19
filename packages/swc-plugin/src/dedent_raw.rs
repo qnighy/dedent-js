5ebb3667707d252b07ac6ecc18cf66f80a93745a
Masaki Hara <ackie.h.gmai@gmail.com>
2023-02-19 21:39:13 +0900

    Start porting plugin to SWC

---
+++ packages/swc-plugin/src/dedent_raw.rs
@@ -0,0 +1,205 @@
+// NOTE: as the raw texts come from the source string, which in turn is encoded in UTF-8,
+// they are known to be free of stray surrogates.
+pub(crate) fn dedent_raw<S>(raw: &[S]) -> Vec<String>
+where
+    S: AsRef<str>,
+{
+    // Dedent the second line and later
+    let mut level = usize::MAX / 2;
+    for (i, elem) in raw.iter().enumerate() {
+        let elem: &str = elem.as_ref();
+        let mut last = 0;
+        while let Some(pos) = find_newline(elem, last) {
+            let end_pos = find_non_space(elem, pos);
+            let len = end_pos - pos;
+            let has_content =
+                if end_pos < elem.len() {
+                    // There is a character in the line
+                    !has_newline_at(elem, end_pos)
+                } else {
+                    // There is a substitution
+                    i + 1 < raw.len()
+                };
+            if has_content {
+                level = level.min(len);
+            }
+            last = end_pos;
+        }
+    }
+    let level = level;
+    let mut dedented = raw.iter().map(|elem| {
+        let elem: &str = elem.as_ref();
+        let mut buf = String::with_capacity(elem.len());
+        let mut last = 0;
+        while let Some(pos) = find_newline(elem, last) {
+            buf.push_str(&elem[last..pos]);
+            let end_pos = find_non_space(elem, pos);
+            let len = end_pos - pos;
+            let trimmed_pos = pos + len.min(level);
+            buf.push_str(&elem[trimmed_pos..end_pos]);
+
+            last = end_pos;
+        }
+        buf.push_str(&elem[last..]);
+        buf
+    }).collect::<Vec<_>>();
+
+    {
+        // Dedent the first line and remove it if it is empty
+        // First line indentation is independent of the later lines.
+        let elem = &dedented[0];
+        let pos = find_non_space(elem, 0);
+        let end_pos = pos + check_newline_at(elem, pos).unwrap_or(0);
+        dedented[0].drain(..end_pos);
+    }
+    dedented
+}
+
+fn find_newline(s: &str, from: usize) -> Option<usize>
+{
+    let s: &[u8] = s.as_ref();
+    let mut i = from;
+    while i < s.len() {
+        if let Some(len) = check_newline_at(s, i) {
+            return Some(i + len);
+        }
+        i += 1;
+    }
+    None
+}
+
+fn has_newline_at(s: &str, i: usize) -> bool
+{
+    check_newline_at(s, i).is_some()
+}
+
+fn check_newline_at<S>(s: &S, i: usize) -> Option<usize>
+where S: AsRef<[u8]> + ?Sized
+{
+    let s: &[u8] = s.as_ref();
+    for &needle in &["\n", "\u{2028}", "\u{2029}"] {
+        if s[i..].starts_with(needle.as_bytes()) {
+            return Some(needle.len());
+        }
+    }
+    None
+}
+
+fn find_non_space(s: &str, from: usize) -> usize
+{
+    let s: &[u8] = s.as_ref();
+    let mut i = from;
+    while i < s.len() && (s[i] == b' ' || s[i] == b'\t') {
+        i += 1;
+    }
+    i
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_remove_initial_spaces() {
+        assert_eq!(dedent_raw(&["    foo bar "]), ["foo bar "]);
+    }
+    #[test]
+    fn test_remove_initial_tabs() {
+        assert_eq!(dedent_raw(&["\tfoo\tbar\t"]), ["foo\tbar\t"]);
+    }
+    #[test]
+    fn test_remove_the_first_line_if_it_is_empty() {
+        assert_eq!(dedent_raw(&["\nfoo bar "]), ["foo bar "]);
+    }
+    #[test]
+    fn test_remove_the_first_line_if_it_only_contains_spaces() {
+        assert_eq!(dedent_raw(&["    \nfoo bar "]), ["foo bar "]);
+    }
+    #[test]
+    fn test_remove_the_first_line_if_it_only_contains_tabs() {
+        assert_eq!(dedent_raw(&["\t\nfoo bar "]), ["foo bar "]);
+    }
+    #[test]
+    fn test_keep_the_last_newlines() {
+        assert_eq!(dedent_raw(&["x\n\n\n"]), ["x\n\n\n"]);
+    }
+    #[test]
+    fn test_keep_spaces_after_substitution() {
+        assert_eq!(dedent_raw(&["  foo", " bar"]), ["foo", " bar"]);
+    }
+    #[test]
+    fn test_keep_spaces_after_bar() {
+        assert_eq!(dedent_raw(&["  foo", " bar"]), ["foo", " bar"]);
+    }
+    #[test]
+    fn test_dedent_the_second_line_without_substitution() {
+        assert_eq!(dedent_raw(&["foo\n  bar "]), ["foo\nbar "]);
+    }
+    #[test]
+    fn test_dedent_the_second_line_with_substitution() {
+        assert_eq!(dedent_raw(&["foo", "foo\n  bar "]), ["foo", "foo\nbar "]);
+    }
+    #[test]
+    fn test_dedent_the_second_line_tab_case_without_substitution() {
+        assert_eq!(dedent_raw(&["foo\n\t\tbar\t"]), ["foo\nbar\t"]);
+    }
+    #[test]
+    fn test_dedent_the_second_line_tab_case_with_substitution() {
+        assert_eq!(dedent_raw(&["foo", "foo\n\t\tbar\t"]), ["foo", "foo\nbar\t"]);
+    }
+    #[test]
+    fn test_dedent_with_variable_indentation_1() {
+        assert_eq!(dedent_raw(&["foo\nbar"]), ["foo\nbar"]);
+    }
+    #[test]
+    fn test_dedent_with_variable_indentation_2() {
+        assert_eq!(dedent_raw(&["foo\n bar"]), ["foo\nbar"]);
+    }
+    #[test]
+    fn test_dedent_with_variable_indentation_3() {
+        assert_eq!(dedent_raw(&["foo\n  bar"]), ["foo\nbar"]);
+    }
+    #[test]
+    fn test_dedent_with_variable_indentation_4() {
+        assert_eq!(dedent_raw(&["foo\n   bar"]), ["foo\nbar"]);
+    }
+    #[test]
+    fn test_dedent_lines_using_max_common_indent_1() {
+        assert_eq!(dedent_raw(&["foo\n  bar\n    baz"]), ["foo\nbar\n  baz"]);
+    }
+    #[test]
+    fn test_dedent_lines_using_max_common_indent_2() {
+        assert_eq!(dedent_raw(&["foo\n    bar\n  baz"]), ["foo\n  bar\nbaz"]);
+    }
+    #[test]
+    fn test_truncate_short_empty_line_in_the_middle() {
+        assert_eq!(dedent_raw(&["foo\n \n  bar\n    baz"]), [
+            "foo\n\nbar\n  baz",
+        ]);
+    }
+    #[test]
+    fn test_truncate_short_empty_line_in_the_last() {
+        assert_eq!(dedent_raw(&["foo\n  bar\n    baz\n "]), [
+            "foo\nbar\n  baz\n",
+        ]);
+    }
+    #[test]
+    fn test_regard_substitution_as_a_content() {
+        assert_eq!(dedent_raw(&["foo\n ", "\n  bar\n    baz"]), [
+            "foo\n",
+            "\n bar\n   baz",
+        ]);
+    }
+    #[test]
+    fn test_dedent_long_empty_line() {
+        assert_eq!(dedent_raw(&["foo\n   \n  bar\n    baz"]), [
+            "foo\n \nbar\n  baz",
+        ]);
+    }
+    #[test]
+    fn test_dedent_the_second_line_and_later_at_infinity_if_they_are_all_empty() {
+        assert_eq!(dedent_raw(&["x\n  \u{2028}\t\n   \u{2029}\t\t"]), [
+            "x\n\u{2028}\n\u{2029}",
+        ]);
+    }
+}
--- packages/swc-plugin/src/lib.rs
+++ packages/swc-plugin/src/lib.rs
@@ -1,36 +1,186 @@
-use swc_core::ecma::{
-    ast::Program,
-    transforms::testing::test,
-    visit::{as_folder, FoldWith, VisitMut},
-};
+mod dedent_raw;
+
+use std::collections::HashSet;
+
+use swc_core::common::util::take::Take;
+use swc_core::ecma::ast::{Program, ImportSpecifier, ModuleExportName, ImportNamedSpecifier, Id, TaggedTpl, Expr, MemberProp, Lit, Callee, ExprOrSpread, Module};
+use swc_core::ecma::transforms::testing::test;
+use swc_core::ecma::visit::{as_folder, FoldWith, VisitMut, VisitMutWith};
+use swc_core::ecma::atoms::{JsWord, Atom};
 use swc_core::plugin::{plugin_transform, proxies::TransformPluginProgramMetadata};
 
-pub struct TransformVisitor;
+use crate::dedent_raw::dedent_raw;
 
-impl VisitMut for TransformVisitor {
-    // Implement necessary visit_mut_* methods for actual custom transform.
-    // A comprehensive list of possible visitor methods can be found here:
-    // https://rustdoc.swc.rs/swc_ecma_visit/trait.VisitMut.html
-}
-
-/// An example plugin function with macro support.
-/// `plugin_transform` macro interop pointers into deserialized structs, as well
-/// as returning ptr back to host.
-///
-/// It is possible to opt out from macro by writing transform fn manually
-/// if plugin need to handle low-level ptr directly via
-/// `__transform_plugin_process_impl(
-///     ast_ptr: *const u8, ast_ptr_len: i32,
-///     unresolved_mark: u32, should_enable_comments_proxy: i32) ->
-///     i32 /*  0 for success, fail otherwise.
-///             Note this is only for internal pointer interop result,
-///             not actual transform result */`
-///
-/// This requires manual handling of serialization / deserialization from ptrs.
-/// Refer swc_plugin_macro to see how does it work internally.
 #[plugin_transform]
 pub fn process_transform(program: Program, _metadata: TransformPluginProgramMetadata) -> Program {
-    program.fold_with(&mut as_folder(TransformVisitor))
+    program.fold_with(&mut as_folder(MainVisitor::new()))
+}
+
+struct MainVisitor {
+    ids: Ids,
+}
+
+impl MainVisitor {
+    fn new() -> Self {
+        Self {
+            ids: Ids::new(),
+        }
+    }
+}
+
+impl VisitMut for MainVisitor {
+    fn visit_mut_module(&mut self, n: &mut Module) {
+        let imports = collect_imports(&self.ids, &n);
+        if !imports.is_empty() {
+            TransformVisitor { ids: self.ids.clone(), imports }.visit_mut_module(n);
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Ids {
+    dedent: JsWord,
+    qnighy_dedent: JsWord,
+}
+
+impl Ids {
+    fn new() -> Self {
+        Self {
+            dedent: JsWord::from("dedent"),
+            qnighy_dedent: JsWord::from("@qnighy/dedent"),
+        }
+    }
+}
+
+struct TransformVisitor {
+    ids: Ids,
+    imports: Imports,
+}
+
+impl VisitMut for TransformVisitor {
+    fn visit_mut_expr(&mut self, n: &mut Expr) {
+        n.visit_mut_children_with(self);
+        let Expr::TaggedTpl(ttpl) = n else {
+            return;
+        };
+        if !self.imports.is_dedent_fn(&self.ids, &ttpl.tag) {
+            return;
+        }
+        let ttpl = n.take().tagged_tpl().unwrap();
+        let mut tpl = ttpl.tpl;
+
+        let quasis_orig = tpl.quasis.iter().map(|elem| elem.raw.to_owned()).collect::<Vec<_>>();
+        let quasis = dedent_raw(&quasis_orig);
+        for (elem, new_quasi) in tpl.quasis.iter_mut().zip(&quasis) {
+            elem.raw = Atom::new(new_quasi.as_str());
+            // TODO: compute cooked correctly
+            elem.cooked = None;
+        }
+
+        *n = Expr::Tpl(tpl);
+    }
+    fn visit_mut_tagged_tpl(&mut self, n: &mut TaggedTpl) {
+        n.visit_mut_children_with(self);
+        let Expr::Call(tag_orig) = n.tag.unwrap_parens() else {
+            return;
+        };
+        let Callee::Expr(callee) = &tag_orig.callee else {
+            return;
+        };
+        if !self.imports.is_dedent_fn(&self.ids, callee) {
+            return;
+        }
+        if !matches!(tag_orig.args[..], [ExprOrSpread { spread: None, expr: _ }]) {
+            return;
+        }
+
+        let mut tag_orig = n.tag.unwrap_parens_mut().take().call().unwrap();
+        let arg = tag_orig.args.swap_remove(0).expr;
+        n.tag = arg;
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Imports {
+    dedent: HashSet<Id>,
+    ns: HashSet<Id>,
+}
+
+impl Imports {
+    fn is_empty(&self) -> bool {
+        self.dedent.is_empty() && self.ns.is_empty()
+    }
+
+    fn is_dedent_fn(&self, ids: &Ids, e: &Expr) -> bool {
+        let e = e.unwrap_parens();
+        match e {
+            Expr::Ident(e) => self.dedent.contains(&e.to_id()),
+            Expr::Member(e) => {
+                if let Some(obj) = e.obj.unwrap_parens().as_ident() {
+                    self.ns.contains(&obj.to_id()) && member_name(&e.prop).map(|name| *name == ids.dedent).unwrap_or(false)
+                } else {
+                    false
+                }
+            }
+            _ => false,
+        }
+    }
+}
+
+fn collect_imports(ids: &Ids, module: &Module) -> Imports {
+    let mut imports = Imports {
+        dedent: HashSet::new(),
+        ns: HashSet::new(),
+    };
+    for stmt in &module.body {
+        let Some(decl) = stmt.as_module_decl() else {
+            continue;
+        };
+        let Some(decl) = decl.as_import() else {
+            continue;
+        };
+        if decl.src.value != ids.qnighy_dedent {
+            continue;
+        }
+        for spec in &decl.specifiers {
+            match spec {
+                ImportSpecifier::Named(spec) => {
+                    if *import_name(spec) == ids.dedent {
+                        let local = spec.local.to_id();
+                        imports.dedent.insert(local);
+                    }
+                }
+                ImportSpecifier::Namespace(spec) => {
+                    let local = spec.local.to_id();
+                    imports.ns.insert(local);
+                }
+                _ => {}
+            }
+        }
+    }
+    imports
+}
+
+fn member_name(prop: &MemberProp) -> Option<&JsWord> {
+    match prop {
+        MemberProp::Ident(prop) => Some(&prop.sym),
+        MemberProp::Computed(prop) => match prop.expr.unwrap_parens() {
+            Expr::Lit(Lit::Str(prop)) => Some(&prop.value),
+            _ => None,
+        },
+        _ => None,
+    }
+}
+
+fn import_name(spec: &ImportNamedSpecifier) -> &JsWord {
+    if let Some(imported) = &spec.imported {
+        match &imported {
+            ModuleExportName::Ident(imported) => &imported.sym,
+            ModuleExportName::Str(imported) => &imported.value,
+        }
+    } else {
+        &spec.local.sym
+    }
 }
 
 // An example to test plugin transform.
@@ -39,10 +189,18 @@ pub fn process_transform(program: Program, _metadata: TransformPluginProgramMeta
 // unless explicitly required to do so.
 test!(
     Default::default(),
-    |_| as_folder(TransformVisitor),
-    boo,
+    |_| as_folder(MainVisitor::new()),
+    transform_dedent_calls,
     // Input codes
-    r#"console.log("transform");"#,
+    r#"import { dedent } from "@qnighy/dedent";
+const text = dedent`
+  foo
+  bar
+`;
+"#,
     // Output codes after transformed with plugin
-    r#"console.log("transform");"#
+    r#"import { dedent } from "@qnighy/dedent";
+const text = `foo
+bar
+`;"#
 );
